1324069455e0cd170908f2a247ba63830b89e003
Leeann Saengsavang <leeann.saengsavang@gmail.com>
2023-06-01 19:12:34 -0700

    Added admin JS

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,39 @@
+async function main() {
+  let response = await fetch('http://localhost:3001/listBooks')
+  let books = await response.json()
+  books.forEach(renderBook)
+}
 
-// Your Code Here
+function renderBook(book) {
+  let root = document.querySelector('#root')
+
+  let li = document.createElement('li')
+  li.textContent = book.title
+
+  // Book quantity
+  let quantityInput = document.createElement('input')
+  quantityInput.value = book.quantity
+
+  // Revises quantity 
+  let saveButton = document.createElement('button')
+  saveButton.textContent = 'Save'
+
+  // Updates the book list
+  saveButton.addEventListener('click', () => {
+      fetch('http://localhost:3001/updateBook', {
+          method: 'PATCH',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+              id: book.id,
+              quantity: quantityInput.value
+          })
+      })
+  })
+
+  li.append(quantityInput, saveButton)
+  root.append(li)
+}
+
+main();
